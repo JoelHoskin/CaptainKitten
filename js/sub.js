ec0a20e3bd8068f615acd90ff86b8a532a54294d
Lily Hoskin <lilyhoskin@gmail.com>
2015-08-12 15:08:33 +0100

    Updates to wave and sub movement

--- js/sub.js
+++ js/sub.js
@@ -1,10 +1,12 @@
 var Sub = function(){
-    PIXI.Sprite.call(this, PIXI.Texture.fromImage('img/submersible.png'));
+    var texture = PIXI.Texture.fromImage('img/submersible.png');
+    PIXI.Sprite.call(this, texture);
     // Setup the position and scale of the bunny
     this.position.x = 400;
     this.position.y = 300;
     this.scale.x = 1;
     this.scale.y = 1;
+    this.pivot = new PIXI.Point(200 / 2, 150 / 2);
     this.velocity = {x : 0, y: 0};
 }
 
@@ -16,26 +18,36 @@ function clamp(x, min, max){
 Sub.prototype.update = function(keyboard){
     //console.log("W: " + keyboard.isWPressed());
     //console.log("S: " + keyboard.isSPressed());
-    if(keyboard.isWPressed()){
-        this.velocity.y -= 0.1;
+    /*if(keyboard.isWPressed()){
+        this.velocity.rotation -= 0.1;
     }
 
     if(keyboard.isSPressed()){
-        this.velocity.y += 0.1;
-    }
+        this.velocity.rotation += 0.1;
+    }*/
 
     if(keyboard.isDPressed()){
-        this.velocity.x += 0.1;
+        this.velocity.x += 0.2;
     }
 
     if(keyboard.isAPressed()){
-        this.velocity.x -= 0.1;
+        this.velocity.x -= 0.2;
     }
 
-    this.velocity.y = clamp(this.velocity.y, -2, 2);
+    //Fricton
+    this.velocity.x *= 0.97;
+
+    this.velocity.y = clamp(this.velocity.y, -8, 8);
 
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
+    this.rotation = 0.4 * (this.velocity.x / 6);
+
+    if(this.velocity.x > 0) {
+        this.scale.x = 1;
+    } else if(this.velocity.x < 0) {
+        this.scale.x = -1;
+    }
     //console.log(this.velocity);
     //console.log(this.position);
 };
--- js/wave.js
+++ js/wave.js
@@ -1,24 +1,32 @@
 var Wave = function() {
     PIXI.Graphics.call(this);
-    //this.lineColor(2, 0x000000, 1);
-
 }
 
 Wave.prototype = Object.create(PIXI.Graphics.prototype);
 
+function createWave(frequency, waveSpeed, waveHeight){
+    return function(i, step){
+        return Math.sin(i * frequency) * Math.sin(step * waveSpeed) * waveHeight
+    }
+}
+
 Wave.prototype.update = function(screenWidth, screenHeight, step){
-    var frequency = 10;
-    var waveSpeed = 0.15
-    var waveHeight = 5
-    var level = 40;
+    var level = 90;
+
+    var waves = [];
+    waves.push(createWave(5, 0.08, 9));
+    waves.push(createWave(10, 0.05, 3));
 
     this.clear();
-    this.beginFill(0xFF0000, 0xFFFFFF);
+    this.beginFill(0x0000FF, 0xFFFFFF);
     var points = [0, screenHeight];
-    for(var i = 0; i <= screenWidth; i += 5){
+    for(var i = 0; i <= screenWidth; i += 10){
         points.push(i);
-        points.push((Math.sin(i * frequency) * Math.sin(step * waveSpeed) * waveHeight) + level);
-        //points.push(10);
+        var waveY = 0;
+        waves.forEach(function(waveFunction){
+            waveY += waveFunction(i, step);
+        });
+        points.push(waveY + level);
     }
     points.push(screenWidth);
     points.push(screenHeight);
